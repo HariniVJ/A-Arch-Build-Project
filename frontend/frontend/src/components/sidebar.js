b9e6fdb2211f2b43a348ff34408b0415251f6363
Dayana Kumar <it22178640@my.sliit.lk>
2024-08-28 22:03:03 +0530

    added header css and jsx

--- frontend/frontend/src/components/sidebar.js
+++ frontend/frontend/src/components/sidebar.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './sidebar.css'; // Correct path if the CSS file is in the same directory as sidebar.js
 import { IonIcon } from '@ionic/react';
-import { pieChartOutline, peopleOutline, receiptOutline, peopleCircleOutline, rocketOutline, heartHalfOutline, logOutOutline } from 'ionicons/icons';
+import { pieChartOutline, peopleOutline, receiptOutline, peopleCircleOutline, rocketOutline, heartHalfOutline, logOutOutline, searchOutline, notificationsOutline } from 'ionicons/icons';
 import logo from '../assets/logobgremoved.png';
-
-
 
 const Sidebar = () => {
     return (
         <div className="container">
+            <header className="header">
+                <div className="header-left">
+                    <input type="text" placeholder="Search..." className="search-bar" />
+                </div>
+                <div className="header-right">
+                    <IonIcon icon={notificationsOutline} className="header-icon" />
+                    <IonIcon icon={heartHalfOutline} className="header-icon" />
+                    <span className="profile-name">Akashwaran</span>
+                </div>
+            </header>
             <div className="navigation">
                 <ul>
                     <li>
                         <a href="#">
                             <span className="icon">
-                             {/** */}    <img src={logo} alt="Company Logo" style={{ width: '80px', height: '80px' }} />
+                                <img src={logo} alt="Company Logo" style={{ width: '80px', height: '80px' }} />
                             </span>
                             <span className="title">Project Manager</span>
                         </a>
@@ -57,14 +65,6 @@ const Sidebar = () => {
                                 <IonIcon icon={rocketOutline} />
                             </span>
                             <span className="title">Inspection</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span className="icon">
-                                <IonIcon icon={heartHalfOutline} />
-                            </span>
-                            <span className="title">Profile</span>
                         </a>
                     </li>
                     <li>
