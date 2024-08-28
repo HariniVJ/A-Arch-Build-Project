9f9f6cb00bcc9028e571ecb3076065556e713c9f
Dayana Kumar <it22178640@my.sliit.lk>
2024-08-28 21:30:03 +0530

    added side bar without logo

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import Sidebar from './components/sidebar';
 
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <Sidebar />
+        </div>
+    );
 }
+
 
 export default App;
---
+++ frontend/frontend/src/components/sidebar.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './sidebar.css'; // Correct path if the CSS file is in the same directory as sidebar.js
+import { IonIcon } from '@ionic/react';
+import { pieChartOutline, peopleOutline, receiptOutline, peopleCircleOutline, rocketOutline, heartHalfOutline, logOutOutline } from 'ionicons/icons';
+
+
+const Sidebar = () => {
+    return (
+        <div className="container">
+            <div className="navigation">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span className="icon">
+                             {/**  <img src={logo} alt="Company Logo" style={{ width: '80px', height: '80px' }} />*/}   
+                            </span>
+                            <span className="title">Project Manager</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span className="icon">
+                                <IonIcon icon={pieChartOutline} />
+                            </span>
+                            <span className="title">Control Center</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span className="icon">
+                                <IonIcon icon={peopleOutline} />
+                            </span>
+                            <span className="title">Project Hub</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span className="icon">
+                                <IonIcon icon={receiptOutline} />
+                            </span>
+                            <span className="title">Daily Pulse</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span className="icon">
+                                <IonIcon icon={peopleCircleOutline} />
+                            </span>
+                            <span className="title">Vendor Vault</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span className="icon">
+                                <IonIcon icon={rocketOutline} />
+                            </span>
+                            <span className="title">Inspection</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span className="icon">
+                                <IonIcon icon={heartHalfOutline} />
+                            </span>
+                            <span className="title">Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span className="icon">
+                                <IonIcon icon={logOutOutline} />
+                            </span>
+                            <span className="title">Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
